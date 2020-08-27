22851c3e694ea484f8d10b5c3de16b28504c8d86
dusan-milanez <dusan.milanez@yahoo.com>
2020-08-27 04:27:59 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "EU (Frankfurt)",
+        BUCKET: "radni-nalog-app-uploads"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://k4o885uti1.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_acmwLViMB",
+        APP_CLIENT_ID: "7nifigk4mrehl5uo3ris3mje2b",
+        IDENTITY_POOL_ID: "us-east-2:7235ce67-cf12-4774-8eb8-7f29c8a1f42b"
+    }
+};
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <div className="lander">
+                <h1>Radni nalog</h1>
+                <p>Aplikacija za evidenciju radnih naloga</p>
+            </div>
+        </div>
+    );
+}
