a6d68cd7f33e900c4ea02e79764d254031567e84
dusan-milanez <dusan.milanez@yahoo.com>
2020-08-29 08:49:36 +0200

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,17 +1,46 @@
-export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
+    //STRIPE_KEY: "YOUR_STRIPE_DEV_PUBLIC_KEY",
     s3: {
-        REGION: "EU (Frankfurt)",
-        BUCKET: "radni-nalog-app-uploads"
+        REGION: "us-east-2",
+        BUCKET: "radni-nalog-app-2-api-dev-attachmentsbucket-15235ixrxq6bz"
     },
     apiGateway: {
         REGION: "us-east-2",
-        URL: "https://k4o885uti1.execute-api.us-east-2.amazonaws.com/prod"
+        URL: "https://msosmjro4h.execute-api.us-east-2.amazonaws.com/dev"
     },
     cognito: {
         REGION: "us-east-2",
-        USER_POOL_ID: "us-east-2_acmwLViMB",
-        APP_CLIENT_ID: "7nifigk4mrehl5uo3ris3mje2b",
-        IDENTITY_POOL_ID: "us-east-2:7235ce67-cf12-4774-8eb8-7f29c8a1f42b"
+        USER_POOL_ID: "us-east-2_o0k6naVBU",
+        APP_CLIENT_ID: "69jnrkfvbdfd622f7ttiijfkhd",
+        IDENTITY_POOL_ID: "us-east-2:96d0f883-fd9f-415f-8dde-26a5c20d9aef"
     }
+};
+
+const prod = {
+    //STRIPE_KEY: "YOUR_STRIPE_PROD_PUBLIC_KEY",
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "radni-nalog-app-2-api-prod-attachmentsbucket-h5kz7naoqrif"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://4wda09ibif.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_3qdK1mDVC",
+        APP_CLIENT_ID: "4qpmrib2c6bm6dt4nep097hk14",
+        IDENTITY_POOL_ID: "us-east-2:caa38e1b-d36e-4438-873b-c3df27e408dc"
+    }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
