b813b9c520b6fd9837e1cf0eb4175f448e547b56
dusan-milanez <dusan.milanez@yahoo.com>
2020-08-29 10:06:57 +0200

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,7 @@ import { onError } from "../libs/errorLib";
 import "./Home.css";
 import { API } from "aws-amplify";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
@@ -58,8 +59,16 @@ export default function Home() {
     function renderLander() {
         return (
             <div className="lander">
-                <h1>Radni nalog</h1>
-                <p>Aplikacija za evidenciju radnih naloga</p>
+                <h1>Scratch</h1>
+                <p>A simple note taking app</p>
+                <div>
+                    <Link to="/login" className="btn btn-info btn-lg">
+                        Login
+                    </Link>
+                    <Link to="/signup" className="btn btn-success btn-lg">
+                        Signup
+                    </Link>
+                </div>
             </div>
         );
     }
